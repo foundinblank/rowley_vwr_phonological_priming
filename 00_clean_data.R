c94ef43030659262b1f1bae71f10c0edb9522c60
Adam Stone <adamstone@gmail.com>
2020-04-12 09:30:32 +0100

    Removed unnecessary columns, save to CSV

--- 00_clean_data.R
+++ 00_clean_data.R
@@ -1,4 +1,4 @@
-## Create clean (but full) dataset from raw Excel file 
+## Create clean dataset from raw Excel file 
 
 library(tidyverse)
 library(janitor)
@@ -6,5 +6,19 @@ library(readxl)
 
 # Load data and clean up column names
 df <- read_xls("data/AllMatchedMLDTdata.xls", sheet = "Main Data") %>%
-  clean_names()
+  clean_names() %>%
+  select(-experiment_name,
+         -session,
+         -clock_information,
+         -display_refresh_rate,
+         -experiment_version,
+         -random_seed,
+         -runtime_version,
+         -runtime_version_expected,
+         -session_time,
+         -studio_version,
+         -target_displayed_duration_error_trial,
+         -exclude_too_fast_slow)
 
+# Save to new CSV file
+write_csv(df, "data/cleaned_dataset.csv")
